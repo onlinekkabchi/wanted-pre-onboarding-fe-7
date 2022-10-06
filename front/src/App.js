ce748f6d5b214988b56f5ac11c5f5be50541b633
onlinekkabchi <onlinekkabchi@gmail.com>
2022-10-06 10:34:46 +0900

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+import styled from "styled-component";
+import "./App.css";
+
+const AppContainer = styled.div``;
+
+function App() {
+    return <div className="App"></div>;
+}
+
+export default App;
