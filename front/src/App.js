2c9ada7a085edbd483c8033abb0e14fa184ba775
onlinekkabchi <onlinekkabchi@gmail.com>
2022-10-07 18:50:45 +0900

    feat: Login page 시작

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,23 @@
-import styled from "styled-component";
+import styled from "styled-components";
+import Login from "./component/login";
 import "./App.css";
 
-const AppContainer = styled.div``;
+const AppContainer = styled.div`
+    width: 450px;
+    min-height: 800px;
+    background: #ebebeb;
+    border-radius: 25px;
+    padding: 25px;
+    margin: 45px;
+`;
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <AppContainer>
+            <h2>App</h2>
+            <Login />
+        </AppContainer>
+    );
 }
 
 export default App;
---
+++ front/src/component/login.js
@@ -0,0 +1,15 @@
+export default function Login() {
+    return (
+        <>
+            <div>
+                <p>email</p>
+                <input type="text" />
+            </div>
+            <div>
+                <p>password</p>
+                <input type="text" />
+            </div>
+            <button type="submit">submit</button>
+        </>
+    );
+}
